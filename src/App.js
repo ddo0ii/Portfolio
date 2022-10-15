b05a93a1b0b95b0506ba9fd3b26b609538c50f69
ddo0ii <soyeong5857@gmail.com>
2022-10-15 18:05:23 +0900

    navigation bar 생성 및 icon 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import AppRouter from "./routes/Router";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -6,11 +5,11 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 function App() {
-  const [init, setInit] = useState(false);
-  useEffect(() => {
-    setInit(true);
-  }, []);
-  return <div className="App">{init ? <AppRouter /> : "Initializing.."}</div>;
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
 }
 
 export default App;
--- src/routes/Navigation.js
+++ src/routes/Navigation.js
@@ -1,26 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "../routes/Navigation.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-const Navigation = ({ userObj }) => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/projects">Projects</Link>
-      </li>
-      <li>
-        <Link to="/certificates">Certificates</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </ul>
-  </nav>
-);
+function Navigation() {
+  return (
+    <nav className={styles.navTop}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/certificates">Certificates</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faBars} />
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 export default Navigation;
