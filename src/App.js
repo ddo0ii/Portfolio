94f35ae0cff1239afc7f379fd947e96f06384292
ddo0ii <soyeong5857@gmail.com>
2022-10-15 15:43:08 +0900

    app.js에서 init처럼해도되지만, router에서의 suspense로 하면될듯

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { useEffect, useState } from "react";
 import AppRouter from "./routes/Router";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
-  return (
-    <div className="App">
-      <AppRouter />
-      <h1>Hello</h1>
-      <h4>This is ddo_0ii's Portfolio Site</h4>
-    </div>
-  );
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    setInit(true);
+  }, []);
+  return <div className="App">{init ? <AppRouter /> : "Initializing.."}</div>;
 }
 
 export default App;
