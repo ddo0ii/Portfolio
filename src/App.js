68833c21e3b97bafb58bd0c26fed377a6fd32e27
ddo0ii <soyeong5857@gmail.com>
2022-10-15 19:42:30 +0900

    loader추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import { useEffect, useState } from "react";
 import AppRouter from "./routes/Router";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 function App() {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    setInit(true);
+  }, []);
   return (
     <div className="appA">
-      <div className="appTop">
-        <img src={"./images/HomeTop.JPEG"} alt="Home Page Top Image" />
-        <AppRouter />
-      </div>
+      {init ? (
+        <div className="appTop">
+          <img src={"./images/HomeTop.JPEG"} alt="Home Page Top Image" />
+          <AppRouter />
+        </div>
+      ) : (
+        <Box className="loading" sx={{ display: "flex" }}>
+          <CircularProgress color="inherit" />
+        </Box>
+      )}
     </div>
   );
 }
