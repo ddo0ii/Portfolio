f678ec3d6def518b9297e19584340727a0ec4521
ddo0ii <soyeong5857@gmail.com>
2022-11-03 16:25:45 +0900

    Timeline 컴포넌트화 - prop으로 넘기기

--- src/components/CareerTimeline.js
+++ src/components/CareerTimeline.js
@@ -6,7 +6,7 @@ import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
 import { DevicesOutlined } from "@mui/icons-material";
-import { Box, Chip, Typography } from "@mui/material";
+import CareerTimelineContent from "./CareerTimelineContent";
 
 function CareerTimeline() {
   return (
@@ -25,58 +25,15 @@ function CareerTimeline() {
           </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent sx={{ py: "12px", px: 2 }}>
-          <Box sx={{ mb: 2 }}>
-            <Chip
-              label="March 2015 - February 2020"
-              size="small"
-              sx={{
-                backgroundColor: "#252525",
-                color: "#DEDEDE",
-                fontSize: "10px",
-                fontWeight: "bold",
-              }}
-            />
-          </Box>
-          <Typography
-            sx={{ typography: { sm: "h6", xs: "subtitle1" } }}
-            component="span"
-          >
-            Handong Global Univ.
-          </Typography>
-          <Typography
-            color="#959595"
-            sx={{ typography: { sm: "body1", xs: "body2" } }}
-          >
-            Computer Engineering (Science and Electrical Engineering)
-          </Typography>
-          <Box color="#959595" sx={{ mt: 2, ml: 1 }}>
-            <Typography sx={{ fontSize: { sm: "14px", xs: "11px" } }}>
-              시선 (소외된 이웃을 위한 IT 솔루션 연구 학회)
-            </Typography>
-            <Typography
-              sx={{
-                fontWeight: "100",
-                fontSize: { sm: "11px", xs: "10px" },
-                mb: 1,
-              }}
-            >
-              March 2017 - July 2018
-            </Typography>
-            <Typography sx={{ fontSize: { sm: "14px", xs: "11px" } }}>
-              Student for Christ(학생신앙운동) 동아리
-            </Typography>
-            <Typography
-              sx={{
-                fontWeight: "100",
-                fontSize: { sm: "11px", xs: "10px" },
-                mb: 1,
-              }}
-            >
-              March 2015 - February 2020
-            </Typography>
-          </Box>
-        </TimelineContent>
+        <CareerTimelineContent
+          timeDate="March 2015 - February 2020"
+          timeTitle="Handong Global Univ."
+          timeSubTitle=" Computer Engineering (Science and Electrical Engineering)"
+          content1="시선 (소외된 이웃을 위한 IT 솔루션 연구 학회)"
+          content1Date="March 2017 - July 2018"
+          content2="Student for Christ(학생신앙운동) 동아리"
+          content2Date="March 2015 - February 2020"
+        />
       </TimelineItem>
       <TimelineItem>
         <TimelineSeparator>
---
+++ src/components/CareerTimelineContent.js
@@ -0,0 +1,69 @@
+import React from "react";
+import TimelineContent from "@mui/lab/TimelineContent";
+import { Box, Chip, Typography } from "@mui/material";
+
+function CareerTimelineContent({
+  timeDate,
+  timeTitle,
+  timeSubTitle,
+  content1,
+  content1Date,
+  content2,
+  content2Date,
+}) {
+  return (
+    <TimelineContent sx={{ py: "12px", px: 2 }}>
+      <Box sx={{ mb: 2 }}>
+        <Chip
+          label={timeDate}
+          size="small"
+          sx={{
+            backgroundColor: "#252525",
+            color: "#DEDEDE",
+            fontSize: "10px",
+            fontWeight: "bold",
+          }}
+        />
+      </Box>
+      <Typography
+        sx={{ typography: { sm: "h6", xs: "subtitle1" } }}
+        component="span"
+      >
+        {timeTitle}
+      </Typography>
+      <Typography
+        color="#959595"
+        sx={{ typography: { sm: "body1", xs: "body2" } }}
+      >
+        {timeSubTitle}
+      </Typography>
+      <Box color="#959595" sx={{ mt: 2, ml: 1 }}>
+        <Typography sx={{ fontSize: { sm: "14px", xs: "11px" } }}>
+          {content1}
+        </Typography>
+        <Typography
+          sx={{
+            fontWeight: "100",
+            fontSize: { sm: "11px", xs: "10px" },
+            mb: 1,
+          }}
+        >
+          {content1Date}
+        </Typography>
+        <Typography sx={{ fontSize: { sm: "14px", xs: "11px" } }}>
+          {content2}
+        </Typography>
+        <Typography
+          sx={{
+            fontWeight: "100",
+            fontSize: { sm: "11px", xs: "10px" },
+            mb: 1,
+          }}
+        >
+          {content2Date}
+        </Typography>
+      </Box>
+    </TimelineContent>
+  );
+}
+export default CareerTimelineContent;
