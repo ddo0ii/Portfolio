4d699d9f1b246af04d7dfb0e78cd92bcd763e2f1
ddo0ii <soyeong5857@gmail.com>
2022-11-08 00:41:06 +0900

    Custom image list - 별로

--- src/components/ActivitiesList.js
+++ src/components/ActivitiesList.js
@@ -5,34 +5,62 @@ import {
   ImageListItem,
   ImageListItemBar,
 } from "@mui/material";
-import InfoIcon from "@mui/icons-material/Info";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import { width } from "@mui/system";
+
+function srcset(image, width, height, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${width * cols}&h=${
+      height * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
 
 function ActivitiesList() {
   return (
-    <ImageList>
-      <ImageListItem key="Subheader" cols={3}></ImageListItem>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={item.author}
-            actionIcon={
-              <IconButton
-                sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                aria-label={`info about ${item.title}`}
-              >
-                <InfoIcon />
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-      ))}
+    <ImageList
+      sx={{
+        // width: 500,
+        // height: 450,
+        // Promote the list into its own layer in Chrome. This costs memory, but helps keeping high FPS.
+        transform: "translateZ(0)",
+      }}
+      //   rowHeight={200}
+      gap={10}
+    >
+      {itemData.map((item) => {
+        const cols = item.featured ? 3 : 1;
+        const rows = item.featured ? 1 : 1;
+
+        return (
+          <ImageListItem key={item.img} cols={cols} rows={rows}>
+            <img
+              {...srcset(item.img, 350, 200, rows, cols)}
+              alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              sx={{
+                background:
+                  "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " +
+                  "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+              }}
+              title={item.title}
+              position="top"
+              actionIcon={
+                <IconButton
+                  sx={{ color: "white" }}
+                  aria-label={`star ${item.title}`}
+                >
+                  <StarBorderIcon />
+                </IconButton>
+              }
+              actionPosition="left"
+            />
+          </ImageListItem>
+        );
+      })}
     </ImageList>
   );
 }
@@ -42,10 +70,8 @@ export default ActivitiesList;
 const itemData = [
   {
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Student for Christ(학생신앙운동) 동아리",
-    author: "Mar 2015 - Feb 2020",
-    // rows: 2,
-    // cols: 2,
+    title: "Breakfast",
+    author: "@bkristastucchio",
     featured: true,
   },
   {
@@ -62,21 +88,16 @@ const itemData = [
     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
     title: "Coffee",
     author: "@nolanissac",
-    // cols: 2,
   },
   {
     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
     title: "Hats",
     author: "@hjrc33",
-    // cols: 2,
   },
   {
     img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
     title: "Honey",
     author: "@arwinneil",
-    // rows: 2,
-    // cols: 2,
-    featured: true,
   },
   {
     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
@@ -87,13 +108,12 @@ const itemData = [
     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
     title: "Fern",
     author: "@katie_wasserman",
+    featured: true,
   },
   {
     img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
     title: "Mushrooms",
     author: "@silverdalex",
-    // rows: 2,
-    // cols: 2,
   },
   {
     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
@@ -109,6 +129,5 @@ const itemData = [
     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
     title: "Bike",
     author: "@southside_customs",
-    // cols: 2,
   },
 ];
