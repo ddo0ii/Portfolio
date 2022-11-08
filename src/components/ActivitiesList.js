5fe7a90075dd91c442d4db8069af6fe1e1b15036
ddo0ii <soyeong5857@gmail.com>
2022-11-08 14:50:06 +0900

    Activities 레이아웃 정리

--- src/components/ActivitiesList.js
+++ src/components/ActivitiesList.js
@@ -16,18 +16,24 @@ function ActivitiesList() {
     <ImageList cols={matches ? 3 : 1}>
       {itemData.map((item) => (
         <ImageListItem key={item.img}>
-          <img src={item.img} alt={item.title} loading="lazy" />
+          <img
+            src={item.img}
+            alt={item.title}
+            loading="lazy"
+            style={{ filter: "brightness(50%)" }}
+          />
           <ImageListItemBar
             title={item.title}
-            subtitle={item.author}
-            actionIcon={
-              <IconButton
-                sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                aria-label={`info about ${item.title}`}
-              >
-                <InfoIcon />
-              </IconButton>
-            }
+            subtitle={item.actiTime}
+            sx={{ backgroundColor: "transparent" }}
+            // actionIcon={
+            //   <IconButton
+            //     sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+            //     aria-label={`info about ${item.title}`}
+            //   >
+            //     <InfoIcon />
+            //   </IconButton>
+            // }
           />
         </ImageListItem>
       ))}
@@ -41,72 +47,61 @@ const itemData = [
   {
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
     title: "Student for Christ(학생신앙운동) 동아리",
-    author: "Mar 2015 - Feb 2020",
-    rows: 2,
-    cols: 2,
-    featured: true,
+    actiTime: "Mar 2015 - Feb 2020",
   },
   {
     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
     title: "Burger",
-    author: "@rollelflex_graphy726",
+    actiTime: "@rollelflex_graphy726",
   },
   {
     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
     title: "Camera",
-    author: "@helloimnik",
+    actiTime: "@helloimnik",
   },
   {
     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
     title: "Coffee",
-    author: "@nolanissac",
-    cols: 2,
+    actiTime: "@nolanissac",
   },
   {
     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
     title: "Hats",
-    author: "@hjrc33",
-    cols: 2,
+    actiTime: "@hjrc33",
   },
   {
     img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
     title: "Honey",
-    author: "@arwinneil",
-    rows: 2,
-    cols: 2,
-    featured: true,
+    actiTime: "@arwinneil",
   },
   {
     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
     title: "Basketball",
-    author: "@tjdragotta",
+    actiTime: "@tjdragotta",
   },
   {
     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
     title: "Fern",
-    author: "@katie_wasserman",
+    actiTime: "@katie_wasserman",
   },
   {
     img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
     title: "Mushrooms",
-    author: "@silverdalex",
-    rows: 2,
-    cols: 2,
+    actiTime: "@silverdalex",
   },
   {
     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
     title: "Tomato basil",
-    author: "@shelleypauls",
+    actiTime: "@shelleypauls",
   },
   {
     img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
     title: "Sea star",
-    author: "@peterlaster",
+    actiTime: "@peterlaster",
   },
   {
     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
     title: "Bike",
-    author: "@southside_customs",
-    cols: 2,
+    actiTime: "@southside_customs",
   },
 ];
