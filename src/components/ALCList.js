509f1d4f8acf02481ff89704a6b777d344178224
ddo0ii <soyeong5857@gmail.com>
2022-11-08 17:22:32 +0900

    alc mobile size control

--- src/components/ALCList.js
+++ src/components/ALCList.js
@@ -20,34 +20,46 @@ function ALCList() {
         "&:hover": { backgroundColor: "black" },
       }}
     >
-      <Box ml={4} mt={8}>
-        <Stack direction="row" mb={8}>
-          <Avatar sx={{ bgcolor: "#D9D9D9", width: 70, height: 70 }}>
-            <FontAwesomeIcon
-              icon={faTrophy}
-              style={{ color: "black", fontSize: "30px" }}
-            />
+      <Box ml={{ sm: 4, xs: 2 }} mt={{ sm: 8, xs: 2 }}>
+        <Stack direction="row" mb={{ sm: 8, xs: 3 }}>
+          <Avatar
+            sx={{
+              bgcolor: "#D9D9D9",
+              width: { sm: 70, xs: 35 },
+              height: { sm: 70, xs: 35 },
+            }}
+          >
+            <Typography sx={{ fontSize: { sm: "30px", xs: "15px" } }}>
+              <FontAwesomeIcon icon={faTrophy} style={{ color: "black" }} />
+            </Typography>
           </Avatar>
         </Stack>
-        <Typography variant="h6" sx={{ mb: { sm: 1, xs: 2 } }}>
+        <Typography
+          sx={{ fontSize: { sm: "20px", xs: "13px" }, mb: { sm: 1, xs: 0.5 } }}
+        >
           <b>국경 없는 과학자 기술자 회장상</b>
         </Typography>
-        <Box color="#959595" sx={{ mb: { sm: 2, xs: 2 } }}>
-          <Typography sx={{ fontSize: "13px" }}>
+        <Box color="#959595" sx={{ mb: 2 }}>
+          <Typography sx={{ fontSize: { sm: "13px", xs: "10px" } }}>
             제 10회 소외된 90%를 위한 창의설계 경진대회
           </Typography>
-          <Typography sx={{ fontSize: "13px" }}>과학기술정보통신부</Typography>
+          <Typography sx={{ fontSize: { sm: "13px", xs: "10px" } }}>
+            과학기술정보통신부
+          </Typography>
         </Box>
-        <Box className="smallTitle" sx={{ mb: { sm: 5, xs: 3 } }}>
+        <Box className="smallTitle" sx={{ mb: { sm: 5, xs: 2.5 } }}>
           <Divider
             color="white"
             sx={{
               borderBottomWidth: { sm: 3, xs: 1.5 },
               width: { sm: 30, xs: 20 },
-              mr: { sm: 3, xs: 2 },
+              mr: { sm: 3, xs: 1.5 },
             }}
           />
-          <Typography variant="overline" color="white">
+          <Typography
+            color="white"
+            sx={{ fontSize: { sm: "13px", xs: "10px" } }}
+          >
             더보기
           </Typography>
         </Box>
