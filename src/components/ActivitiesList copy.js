e4d1587d878ee77682bc1f20215b1760e1398793
ddo0ii <soyeong5857@gmail.com>
2022-11-08 14:17:39 +0900

    ActivitiesList 화면크기에따라 갯수 다르게

--- src/components/ActivitiesList copy.js
+++ src/components/ActivitiesList copy.js
@@ -6,17 +6,19 @@ import {
   ImageListItemBar,
 } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
-import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
 
 function ActivitiesList() {
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up("sm"));
   return (
-    <ImageList cols={matches ? 3 : 1}>
+    <ImageList>
+      <ImageListItem key="Subheader" cols={3}></ImageListItem>
       {itemData.map((item) => (
         <ImageListItem key={item.img}>
-          <img src={item.img} alt={item.title} loading="lazy" />
+          <img
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
           <ImageListItemBar
             title={item.title}
             subtitle={item.author}
@@ -42,8 +44,8 @@ const itemData = [
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
     title: "Student for Christ(학생신앙운동) 동아리",
     author: "Mar 2015 - Feb 2020",
-    rows: 2,
-    cols: 2,
+    // rows: 2,
+    // cols: 2,
     featured: true,
   },
   {
@@ -60,20 +62,20 @@ const itemData = [
     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
     title: "Coffee",
     author: "@nolanissac",
-    cols: 2,
+    // cols: 2,
   },
   {
     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
     title: "Hats",
     author: "@hjrc33",
-    cols: 2,
+    // cols: 2,
   },
   {
     img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
     title: "Honey",
     author: "@arwinneil",
-    rows: 2,
-    cols: 2,
+    // rows: 2,
+    // cols: 2,
     featured: true,
   },
   {
@@ -90,8 +92,8 @@ const itemData = [
     img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
     title: "Mushrooms",
     author: "@silverdalex",
-    rows: 2,
-    cols: 2,
+    // rows: 2,
+    // cols: 2,
   },
   {
     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
@@ -107,6 +109,6 @@ const itemData = [
     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
     title: "Bike",
     author: "@southside_customs",
-    cols: 2,
+    // cols: 2,
   },
 ];
