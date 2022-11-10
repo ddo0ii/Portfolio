8d2ca0fc51b1ee4debfc427455964bf1248c8817
ddo0ii <soyeong5857@gmail.com>
2022-11-10 15:42:43 +0900

    Project Dialog Top

--- src/components/ProjectDialog.js
+++ src/components/ProjectDialog.js
@@ -8,31 +8,46 @@ function ProjectDialog() {
         <Container>
           <Typography
             align="center"
-            gutterBottom
-            color="primary"
-            sx={{ typography: { sm: "h1", xs: "h4" } }}
+            color="#989796"
+            sx={{
+              fontSize: { sm: "14px", xs: "7px" },
+              letterSpacing: "4px",
+              mt: { sm: 20, xs: 10 },
+            }}
+          >
+            PROJECT
+          </Typography>
+          <Typography
+            align="center"
+            color="secondary"
+            sx={{
+              // typography: { sm: "h2", xs: "h5" },
+              fontSize: { sm: "80px", xs: "30px" },
+              letterSpacing: { sm: "5px", xs: "2px" },
+              mt: { sm: 2, xs: 1 },
+            }}
           >
             <b>부동산 계산기</b>
           </Typography>
           <Typography
             align="center"
-            gutterBottom
-            color="secondary"
+            color="#989796"
             sx={{
-              typography: { sm: "h4", xs: "body1" },
+              typography: { sm: "h5", xs: "body2" },
+              mt: { sm: 3, xs: 2 },
             }}
           >
             케이페이스 인턴(개발팀)
           </Typography>
           <Typography
             align="center"
-            gutterBottom
-            color="secondary"
+            color="#989796"
             sx={{
-              typography: { sm: "h4", xs: "body1" },
+              typography: { sm: "h6", xs: "overline" },
+              mb: { sm: 15, xs: 8 },
             }}
           >
-            DECEMBER 2019 - February 2020
+            DECEMBER 2019 - FEBRUARY 2020
           </Typography>
         </Container>
       </Box>
