f070246f4154ecde34bb850a0f1e8e7a8d163d89
ddo0ii <soyeong5857@gmail.com>
2022-11-30 15:48:47 +0900

    Activities 구조 변경

---
+++ src/components/ActivitiesContent.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  ImageListItem,
+  ImageListItemBar,
+  Paper,
+} from "@mui/material";
+import ActivitiesDialog from "./ActivitiesDialog";
+import { Box } from "@mui/system";
+
+function ActivitiesContent({
+  id,
+  actImage,
+  actTitle,
+  actSubTitle,
+  actOrganizer,
+  actDate,
+  actDeTitle1,
+  actDeContent1_1,
+  actDeContent1_2,
+  actDeContent1_3,
+  actDeContent1_4,
+  actDeTitle2,
+  actDeContent2,
+  actDeTitle3,
+  actDeContent3,
+  actDeTitle4,
+  actDeContent4,
+  actDef,
+  actPrototypeAlt,
+  actPrototype,
+}) {
+  const [open, setOpen] = useState(false);
+  const [scroll, setScroll] = useState("paper");
+
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = useRef(null);
+  useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <Box key={id}>
+      <ImageListItem onClick={handleClickOpen("body")}>
+        <img
+          src={process.env.PUBLIC_URL + actImage}
+          alt={actTitle}
+          loading="lazy"
+          style={{ filter: "brightness(30%)" }}
+        />
+        <ImageListItemBar
+          title={actTitle}
+          subtitle={actSubTitle}
+          sx={{ backgroundColor: "transparent" }}
+        />
+      </ImageListItem>
+      <Paper elevation={10}>
+        <Dialog
+          // fullWidth={true}
+          maxWidth="xl"
+          open={open}
+          onClose={handleClose}
+          scroll={scroll}
+        >
+          <ActivitiesDialog
+            id={id}
+            actImage={actImage}
+            actTitle={actTitle}
+            actSubTitle={actSubTitle}
+            actOrganizer={actOrganizer}
+            actDate={actDate}
+            actDeTitle1={actDeTitle1}
+            actDeContent1_1={actDeContent1_1}
+            actDeContent1_2={actDeContent1_2}
+            actDeContent1_3={actDeContent1_3}
+            actDeContent1_4={actDeContent1_4}
+            actDeTitle2={actDeTitle2}
+            actDeContent2={actDeContent2}
+            actDeTitle3={actDeTitle3}
+            actDeContent3={actDeContent3}
+            actDeTitle4={actDeTitle4}
+            actDeContent4={actDeContent4}
+            actDef={actDef}
+            actPrototypeAlt={actPrototypeAlt}
+            actPrototype={actPrototype}
+          />
+          <DialogActions sx={{ backgroundColor: "#0F0F0F" }}>
+            <Button onClick={handleClose}>Close</Button>
+          </DialogActions>
+        </Dialog>
+      </Paper>
+    </Box>
+  );
+}
+
+export default ActivitiesContent;
