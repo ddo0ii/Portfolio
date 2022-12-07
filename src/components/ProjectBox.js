232fcd85ee7f10d294b57fc43138924c79c4f756
ddo0ii <soyeong5857@gmail.com>
2022-12-07 14:11:37 +0900

    새로운 모듈 적용

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -16,7 +16,13 @@ import {
   LazyLoadComponent,
   LazyLoadImage,
 } from "react-lazy-load-image-component";
-import useScrollClipPath from "../hooks/useScrollClipPath";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const boxVariant = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  hidden: { opacity: 0, scale: 0 },
+};
 
 function ProjectBox({
   id,
@@ -69,113 +75,135 @@ function ProjectBox({
     }
   }, [open]);
 
-  const animatedImage = useScrollClipPath();
+  const AnimationBox = () => {
+    const control = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+      if (inView) {
+        control.start("visible");
+      } else {
+        control.start("hidden");
+      }
+    }, [control, inView]);
+
+    return (
+      <motion.div
+        className="box"
+        ref={ref}
+        variants={boxVariant}
+        initial="hidden"
+        animate={control}
+      >
+        <Card
+          className="cardContent"
+          elevation={10}
+          onClick={handleClickOpen("body")}
+          sx={{
+            //maxWidth: 300,
+            borderRadius: 0,
+            filter: "brightness(50%)",
+          }}
+        >
+          <CardActionArea>
+            <CardMedia className="sizingUpImg" height="300">
+              <LazyLoadImage
+                src={pjSImage}
+                alt={`${pjTitle} Image`}
+                effect="blur"
+                width="100%"
+                height="300"
+                style={{ margin: 0, padding: 0 }}
+              />
+            </CardMedia>
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                {pjTitle}
+              </Typography>
+              <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+                {pjDeContent1_1 ? (
+                  <Box
+                    color="text.secondary"
+                    sx={{
+                      border: 1,
+                      borderColor: "#E8E8E8",
+                      p: "2px",
+                      pl: 1,
+                      pr: 1,
+                      mr: 1,
+                      mb: 1,
+                    }}
+                  >
+                    {pjDeContent1_1}
+                  </Box>
+                ) : (
+                  ""
+                )}
+                {pjDeContent1_2 ? (
+                  <Box
+                    color="text.secondary"
+                    sx={{
+                      border: 1,
+                      borderColor: "#E8E8E8",
+                      p: "2px",
+                      pl: 1,
+                      pr: 1,
+                      mr: 1,
+                      mb: 1,
+                    }}
+                  >
+                    {pjDeContent1_2}
+                  </Box>
+                ) : (
+                  ""
+                )}
+                {pjDeContent1_3 ? (
+                  <Box
+                    color="text.secondary"
+                    sx={{
+                      border: 1,
+                      borderColor: "#E8E8E8",
+                      p: "2px",
+                      pl: 1,
+                      pr: 1,
+                      mr: 1,
+                      mb: 1,
+                    }}
+                  >
+                    {pjDeContent1_3}
+                  </Box>
+                ) : (
+                  ""
+                )}
+                {pjDeContent1_4 ? (
+                  <Box
+                    color="text.secondary"
+                    sx={{
+                      border: 1,
+                      borderColor: "#E8E8E8",
+                      p: "2px",
+                      pl: 1,
+                      pr: 1,
+                      mr: 1,
+                      mb: 1,
+                    }}
+                  >
+                    {pjDeContent1_4}
+                  </Box>
+                ) : (
+                  ""
+                )}
+              </Box>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </motion.div>
+    );
+  };
 
   return (
     <Box>
-      <Card
-        {...animatedImage}
-        className="cardContent"
-        elevation={10}
-        onClick={handleClickOpen("body")}
-        sx={{
-          //maxWidth: 300,
-          borderRadius: 0,
-          filter: "brightness(50%)",
-        }}
-      >
-        <CardActionArea>
-          <CardMedia className="sizingUpImg" height="300">
-            <LazyLoadImage
-              src={pjSImage}
-              alt={`${pjTitle} Image`}
-              effect="blur"
-              width="100%"
-              height="300"
-              style={{ margin: 0, padding: 0 }}
-            />
-          </CardMedia>
-          <CardContent>
-            <Typography gutterBottom variant="h6" component="div">
-              {pjTitle}
-            </Typography>
-            <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-              {pjDeContent1_1 ? (
-                <Box
-                  color="text.secondary"
-                  sx={{
-                    border: 1,
-                    borderColor: "#E8E8E8",
-                    p: "2px",
-                    pl: 1,
-                    pr: 1,
-                    mr: 1,
-                    mb: 1,
-                  }}
-                >
-                  {pjDeContent1_1}
-                </Box>
-              ) : (
-                ""
-              )}
-              {pjDeContent1_2 ? (
-                <Box
-                  color="text.secondary"
-                  sx={{
-                    border: 1,
-                    borderColor: "#E8E8E8",
-                    p: "2px",
-                    pl: 1,
-                    pr: 1,
-                    mr: 1,
-                    mb: 1,
-                  }}
-                >
-                  {pjDeContent1_2}
-                </Box>
-              ) : (
-                ""
-              )}
-              {pjDeContent1_3 ? (
-                <Box
-                  color="text.secondary"
-                  sx={{
-                    border: 1,
-                    borderColor: "#E8E8E8",
-                    p: "2px",
-                    pl: 1,
-                    pr: 1,
-                    mr: 1,
-                    mb: 1,
-                  }}
-                >
-                  {pjDeContent1_3}
-                </Box>
-              ) : (
-                ""
-              )}
-              {pjDeContent1_4 ? (
-                <Box
-                  color="text.secondary"
-                  sx={{
-                    border: 1,
-                    borderColor: "#E8E8E8",
-                    p: "2px",
-                    pl: 1,
-                    pr: 1,
-                    mr: 1,
-                    mb: 1,
-                  }}
-                >
-                  {pjDeContent1_4}
-                </Box>
-              ) : (
-                ""
-              )}
-            </Box>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      <AnimationBox></AnimationBox>
       <Paper elevation={10}>
         <Dialog
           //fullWidth={true}
